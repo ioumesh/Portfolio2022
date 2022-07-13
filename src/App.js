25b22e16b45fb476cd29a3367b7453ce03d91a79
ioumesh <umeshkumar@zingbus.com>
2022-07-13 20:46:33 +0530

    feat:portfolio emailjs and swiper added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import "./App.css";
+import Contact from "./component/Contact/Contact";
+import Experience from "./component/Experience/Experience";
+import Footer from "./component/Footer/Footer";
 import Intro from "./component/intro/Intro";
 import Navbar from "./component/navbar/Navbar";
+import Portfolio from "./component/Portfolio/Portfolio";
+import Services from "./component/services/Services";
+import Testimonial from "./component/Testimonials/Testimonial";
+import Works from "./component/Works/Works";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Intro />
+      <Services />
+      <Experience />
+      <Works />
+      <Portfolio />
+      <Testimonial />
+      <Contact />
+      <Footer />
     </div>
   );
 }
